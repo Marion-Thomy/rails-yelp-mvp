22a2080f9ec0a8e6a8ef64dd544a6c8506ca6e50
Marion Thomy <marionthomy@gmail.com>
2020-11-11 17:01:05 +0100

    almost finish exo2 - pb reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,21 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.save
+      redirect_to_root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
